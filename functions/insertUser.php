3176de46fe912af8787aaad977531ded3a0850cb
Cristopher <cristopherquiros2@gmail.com>
2025-10-15 15:10:05 -0600

    Added image upload

--- functions/insertUser.php
+++ functions/insertUser.php
@@ -1,6 +1,9 @@
 <?php
 require('../common/connection.php');
 
+$directorio_subida = '../images/';
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
  
     $cedula = $_POST['cedula'];
@@ -9,20 +12,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $fecha_nac = $_POST['fecha_nac'];
     $correo = $_POST['correo'];
     $telefono = $_POST['telefono'];
-    $foto = $_POST['foto'];
     $password = md5($_POST['password']);
     $estado = 'pendiente';
     $tipo_usuario = 'user';
 
+
+
+    $nombreImagen_DB = null; 
+
+    if (isset($_FILES['foto']) && $_FILES['foto']['error'] === UPLOAD_ERR_OK) {
+        
+        $nombreArchivoUnico = uniqid() . '_' . basename($_FILES['foto']['name']);
+        $rutaTemporal = $_FILES['foto']['tmp_name'];
+        $rutaDestinoFinal = $directorio_subida . $nombreArchivoUnico; 
+        
+   
+        if(move_uploaded_file($rutaTemporal, $rutaDestinoFinal)) {
+            
+            $rutaBaseWeb = '/Aventones/images/'; 
+            $nombreImagen_DB = $rutaBaseWeb . $nombreArchivoUnico;
+
+        } else {
+        
+             echo "ERROR: Falló move_uploaded_file. Revise permisos y rutas de la carpeta.";
+             error_log("Fallo al mover archivo a: " . $rutaDestinoFinal);
+      
+        }
+    }
     $sql = "INSERT INTO usuarios (cedula, name, lastName, birthDate, mail, phoneNum, password, image, state, userType)
-            VALUES ('$cedula', '$name', '$lastName', '$fecha_nac', '$correo', '$telefono', '$password', '$foto', '$estado', '$tipo_usuario')";
+            VALUES ('$cedula', '$name', '$lastName', '$fecha_nac', '$correo', '$telefono', '$password', '$nombreImagen_DB', '$estado', '$tipo_usuario')";
 
     if (mysqli_query($conn, $sql)) {
-        header("Location: /index.php?success=registration_complete");
+        header("Location: /index.php");
         exit();
     } else {
-        header("Location: /index.php?errors=registraton_failed");
-        exit();
+       
     }
 
     mysqli_close($conn);
