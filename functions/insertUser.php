a9fdc34dea92f0b8901ad41784bbf4cdf706b48c
Cristopher <cristopherquiros2@gmail.com>
2025-10-14 23:03:19 -0600

    Password added

--- functions/insertUser.php
+++ functions/insertUser.php
@@ -14,8 +14,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $estado = 'pendiente';
     $tipo_usuario = 'user';
 
-    $sql = "INSERT INTO usuarios (cedula, name, lastName, birthDate, mail, phoneNum, image, state, userType)
-            VALUES ('$cedula', '$name', '$lastName', '$fecha_nac', '$correo', '$telefono', '$foto', '$estado', '$tipo_usuario')";
+    $sql = "INSERT INTO usuarios (cedula, name, lastName, birthDate, mail, phoneNum, password, image, state, userType)
+            VALUES ('$cedula', '$name', '$lastName', '$fecha_nac', '$correo', '$telefono', '$password', '$foto', '$estado', '$tipo_usuario')";
 
     if (mysqli_query($conn, $sql)) {
         header("Location: /index.php?success=registration_complete");
