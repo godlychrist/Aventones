b93c724d1b240a690281220e74d9ac8365b7412a
Cristopher <cristopherquiros2@gmail.com>
2025-10-15 15:10:44 -0600

    Syntaxis Error Fixed

--- index.php
+++ index.php
@@ -1,25 +1,16 @@
-<?php
-  //check if user is logged in
-  //if not, redirect to login page
-  session_start();
-  if (isset($_SESSION['username'])) {
-      header("Location: /pages/dashboard.php");
-      exit();
-  }
-
-?>
+ 
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
-  <link rel="stylesheet" href="css/login.css">
+  <link rel="stylesheet" href="/css/logIn.css">
 </head>
 <body>
   <h1>Login</h1>
   <form action="actions/login.php" method="post" class="login-form">
-    <div class="form-grouper">
+    <div class="form-grouper"> 
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required>
         <br>
