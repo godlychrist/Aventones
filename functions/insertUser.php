f401cf9eba021a1de8369c21dd461abe782c6304
Cristopher <cristopherquiros2@gmail.com>
2025-10-14 22:47:42 -0600

    Name Change, Register Page & Insert (WIP)

---
+++ functions/insertUser.php
@@ -0,0 +1,30 @@
+<?php
+include('/common/connection.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ 
+    $cedula = $_POST['cedula'];
+    $name = $_POST['name'];
+    $lastName = $_POST['lastName'];
+    $fecha_nac = $_POST['fecha_nac'];
+    $correo = $_POST['correo'];
+    $telefono = $_POST['telefono'];
+    $foto = $_POST['foto'];
+    $password = md5($_POST['password']);
+    $estado = 'pendiente';
+    $tipo_usuario = 'user';
+
+    $sql = "INSERT INTO usuarios (cedula, name, lastName, birthDate, mail, phoneNum, image, state, userType)
+            VALUES ('$cedula', '$lastName', '$username', '$fecha_nac', '$correo', '$telefono', '$foto', '$estado', '$tipo_usuario')";
+
+    if (mysqli_query($conn, $sql)) {
+        header("Location: /index.php?success=registration_complete");
+        exit();
+    } else {
+        header("Location: /index.php?errors=registraton_failed");
+        exit();
+    }
+
+    mysqli_close($conn);
+}
+?>
---
+++ pages/registration.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Register</title>
+</head>
+
+<body>
+    <h1>Register</h1>
+    <form action="/actions/insertUser.php" method="post">
+
+<br>
+        <div class="form-grouper">
+            <label for="name">Nombre:</label>
+            <input type="text" id="name" name="name" required><br><br>
+
+            <label for="lastName">Apellidos:</label>
+            <input type="text" id="lastName" name="lastName" required><br><br>
+
+            <label for="cedula">Cedula:</label>
+            <input type="text" id="cedula" name="cedula" required><br><br>
+
+            <label for="fecha_nac">Fecha de Nacimiento:</label>
+            <input type="text" id="fecha_nac" name="fecha_nac" required><br><br>
+        </div>
+        
+        <div class="one-grouper">
+            <label for="correo">Correo:</label>
+            <input type="text" id="correo" name="correo" required><br><br>
+        </div>
+
+        <div class="form-grouper">        
+            <label for="password">Telefono:</label>
+            <input type="text" id="telefono" name="telefono" required><br><br>
+            
+            <label for="password">Fotografía Personal:</label>
+            <input type="text" id="foto" name="foto" required><br><br>
+        
+            <label for="password">Password:</label>
+            <input type="text" id="password" name="password" required><br><br>
+
+            <label for="password">Repeat Password:</label>
+            <input type="password" id="password" name="password" required><br><br>
+        </div>
+        <button type="submit">Register</button>
+    </form>
+</body>
+
+</html>
