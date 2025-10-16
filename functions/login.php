dac397e25c53bd446b5693c3948f3e11ac436566
Cristopher <cristopherquiros2@gmail.com>
2025-10-15 21:26:38 -0600

    Login Done

---
+++ functions/login.php
@@ -0,0 +1,30 @@
+<?php
+include('../common/connection.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $cedula= $_POST['cedula'];
+    $password = $_POST['password'];
+    //check user in the database
+    $sql = "SELECT * FROM usuarios WHERE cedula = '$cedula' AND state = 'activa'";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        if (md5($password) === $row['password']){
+          session_start();
+          $_SESSION['name'] = $row['name'];
+          $_SESSION['lastname'] = $row['lastname'];
+          header("Location: /pages/main.php");
+          exit();
+        } else {
+          header("Location: /index.php");
+        }
+    } else {
+        header("Location: /index.php");
+    }
+    mysqli_close($conn);
+
+} else {
+    header("Location: /index.php");
+    exit();
+}
