84542fb0a55b1e99495e716a41c13458b08bd2eb
Brian Rodríguez <brianrp19@gmail.com>
2025-10-16 23:19:04 -0600

    se le dio funcionalidad a insertar ride

---
+++ functions/insertride.php
@@ -0,0 +1,54 @@
+<?php
+require('../common/connection.php');
+require('sendEmail.php');
+
+$directorio_subida = '../images/';
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ 
+   
+    
+    $nombre = $_POST['name'];
+    $lugardesalida = $_POST['destination'];
+    $lugardellegada = $_POST['arrival'];
+    $horaydia = $_POST['date'];
+    $costoxespacio = $_POST['space_cost'];
+    $numerodeplaca = $_POST['vehicle_id'];
+    $cantidad = $_POST['space'];
+
+
+    $nombreImagen_DB = null; 
+
+    if (isset($_FILES['image']) && $_FILES['image']['error'] === UPLOAD_ERR_OK) {
+        
+        $nombreArchivoUnico = uniqid() . '_' . basename($_FILES['image']['name']);
+        $rutaTemporal = $_FILES['image']['tmp_name'];
+        $rutaDestinoFinal = $directorio_subida . $nombreArchivoUnico; 
+        
+   
+        if(move_uploaded_file($rutaTemporal, $rutaDestinoFinal)) {
+            
+            $rutaBaseWeb = '/Aventones/images/'; 
+            $nombreImagen_DB = $rutaBaseWeb . $nombreArchivoUnico;
+
+        } else {
+        
+             echo "ERROR: Falló move_uploaded_file. Revise permisos y rutas de la carpeta.";
+             error_log("Fallo al mover archivo a: " . $rutaDestinoFinal);
+      
+        }
+    }
+    $sql = "INSERT INTO rides (name, destination, arrival, date, space_cost, space ,user_id, vehicle_id)
+            VALUES ('$nombre', '$lugardesalida', '$lugardellegada', '$horaydia', '$costoxespacio','$cantidad', 1, '$numerodeplaca')";
+
+     if (mysqli_query($conn, $sql)) {
+        header("Location: /index.php");
+        exit();
+    } else {
+       
+    }
+
+    mysqli_close($conn);
+}
+?>
