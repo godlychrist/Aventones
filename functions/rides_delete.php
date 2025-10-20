75f74b927a4220cb0f114b1877341022a2557280
Brian Rodríguez <brianrp19@gmail.com>
2025-10-19 19:26:17 -0600

    se agrega el delete de ride

---
+++ functions/rides_delete.php
@@ -0,0 +1,19 @@
+<?php
+include('../common/connection.php');
+
+
+$rides = $_POST['ride_id'];
+
+// delete user if id is provided from the database
+if ($rides) {
+    $sql = "DELETE FROM rides WHERE id = $rides";
+    if (mysqli_query($conn, $sql)) {
+        header("Location: /pages/ride.php");
+        exit();
+    } else {
+        echo "Error deleting record: " . mysqli_error($conn);
+    }
+}
+mysqli_close($conn);
+
+?>
